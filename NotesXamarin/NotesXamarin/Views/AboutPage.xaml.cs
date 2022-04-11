1f04b10787a380361d3461c53b8b5a1c6f81d225
Bruno Sales <baliestri@icloud.com>
2022-04-11 14:17:08 -0300

    done

---
+++ NotesXamarin/NotesXamarin/Views/AboutPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace NotesXamarin.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnButtonClicked(object sender, EventArgs e)
+        {
+            await Launcher.OpenAsync("https://aka.ms/xamarin-quickstart");
+        }
+    }
+}
---
+++ NotesXamarin/NotesXamarin/Views/NotesPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+using Xamarin.Forms;
+
+namespace NotesXamarin.Views
+{
+    public partial class NotesPage : ContentPage
+    {
+        private readonly string _filename = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "notes.txt");
+
+        public NotesPage()
+        {
+            InitializeComponent();
+
+            if (File.Exists(_filename))
+                editor.Text = File.ReadAllText(_filename);
+        }
+
+        void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            File.WriteAllText(_filename, editor.Text);
+        }
+
+        void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            if (File.Exists(_filename))
+                File.Delete(_filename);
+
+            editor.Text = string.Empty;
+        }
+    }
+}
